443934ab08b3b2c05b017cec27f1614cb0362eed
Garry3Fingers <www.vlad78@yandex.ru>
2024-03-04 20:57:24 +0300

    Add model concern Visible

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  VALID_STATUES = %w[public private archived]
-
-  validates :status, inclusion: { in: VALID_STATUES }
-
-  def archived?
-    status == 'archived'
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,5 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
-
-  VALID_STATUES = %w[public private archived]
-
-  validates :status, inclusion: { in: VALID_STATUES }
-
-  def archived?
-    status == 'archived'
-  end
 end
